f51a84bee138f2095dc4226b4d4c5139cae41fdb
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-08-14 15:33:15 +0000

    add helper fields

---
+++ lib/src/fields/base_fields.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'formatters.dart';
+
+class DoubleInput extends HookWidget {
+  final String label;
+  final void Function(double?) onChanged;
+  final double? value;
+
+  const DoubleInput({
+    Key? key,
+    required this.label,
+    required this.onChanged,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final input = useTextInput<double>(
+      value,
+      onChanged,
+      doubleStringInput,
+    );
+
+    return TextField(
+      controller: input.controller,
+      decoration: InputDecoration(
+        labelText: label,
+        errorText: input.errorIfTouchedNotEmpty,
+      ),
+      onChanged: input.onChangedString,
+      inputFormatters: [Formatters.onlyDigitsOrDecimal],
+      focusNode: input.focusNode,
+      keyboardType: TextInputType.number,
+    );
+  }
+}
+
+class IntInput extends HookWidget {
+  final String label;
+  final void Function(int?) onChanged;
+  final int? value;
+
+  const IntInput({
+    Key? key,
+    required this.label,
+    required this.onChanged,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const _size = 20.0;
+    final input = useTextInput<int>(
+      value,
+      onChanged,
+      intStringInput,
+    );
+    final _buttonStyle = TextButton.styleFrom(
+      padding: EdgeInsets.zero,
+      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      fixedSize: const Size(_size, _size),
+      minimumSize: const Size(_size, _size),
+    );
+
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: input.controller,
+            decoration: InputDecoration(
+              labelText: label,
+              errorText: input.errorIfTouchedNotEmpty,
+            ),
+            onChanged: input.onChangedString,
+            inputFormatters: [Formatters.onlyDigits],
+            focusNode: input.focusNode,
+            keyboardType: TextInputType.number,
+          ),
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextButton(
+              onPressed: value == null
+                  ? null
+                  : () {
+                      onChanged(value! + 1);
+                    },
+              style: _buttonStyle,
+              child: const Icon(Icons.arrow_drop_up, size: _size - 2),
+            ),
+            TextButton(
+              onPressed: value == null
+                  ? null
+                  : () {
+                      onChanged(value! - 1);
+                    },
+              style: _buttonStyle,
+              child: const Icon(Icons.arrow_drop_down, size: _size - 2),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+final doubleStringInput = StringInputSerializer<double>(
+  double.tryParse,
+  (v) {
+    final str = v.toString();
+    return str.endsWith('.0') ? str.substring(0, str.length - 2) : str;
+  },
+);
+
+String objectToString(Object v) => v.toString();
+
+const intStringInput = StringInputSerializer<int>(
+  int.tryParse,
+  objectToString,
+);
+
+class StringInputSerializer<T> {
+  final T? Function(String) fromString;
+  final String Function(T) asString;
+  final String? Function(String, T?)? validate;
+
+  const StringInputSerializer(
+    this.fromString,
+    this.asString, {
+    this.validate,
+  });
+}
+
+TextInputParams useTextInput<T>(
+  T? value,
+  void Function(T?) onChanged,
+  StringInputSerializer<T> serializer,
+) {
+  final controller = useTextEditingController();
+  final focusNode = useFocusNode();
+  final error = useState<String?>(null);
+  final isTouched = useState(false);
+
+  useEffect(() {
+    if (value == null) {
+      controller.value = controller.value.copyWith(text: '');
+    } else if (serializer.fromString(controller.text) != value) {
+      error.value = null;
+      controller.value = controller.value.copyWith(
+        text: serializer.asString(value),
+      );
+    }
+  }, [serializer, value]);
+
+  final onChangedString = useMemoized(() {
+    void _onControllerChange(String newString) {
+      final newValue = serializer.fromString(newString);
+      final newError = serializer.validate?.call(newString, newValue);
+
+      if (newValue != null && newError == null) {
+        if (value != newValue) {
+          onChanged(newValue);
+        }
+        error.value = null;
+      } else if (newString.isEmpty) {
+        if (value != newValue) {
+          onChanged(null);
+        }
+      } else {
+        error.value = newError ?? '';
+      }
+    }
+
+    return _onControllerChange;
+  }, [serializer, value, onChanged]);
+
+  useValueChanged<bool, void>(focusNode.hasPrimaryFocus, (prev, _) {
+    if (prev && !focusNode.hasPrimaryFocus) {
+      isTouched.value = true;
+    }
+  });
+
+  return TextInputParams(
+    controller: controller,
+    focusNode: focusNode,
+    error: error.value,
+    isTouched: isTouched.value,
+    onChangedString: onChangedString,
+  );
+}
+
+class TextInputParams {
+  final TextEditingController controller;
+  final FocusNode focusNode;
+  final String? error;
+  final bool isTouched;
+  final void Function(String) onChangedString;
+
+  const TextInputParams({
+    required this.controller,
+    required this.focusNode,
+    required this.error,
+    required this.isTouched,
+    required this.onChangedString,
+  });
+
+  String? get errorIfTouched => isTouched ? error : null;
+  String? get errorIfTouchedNotEmpty =>
+      isTouched && controller.text.isNotEmpty ? error : null;
+}
---
+++ lib/src/fields/button_select_field.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_hooks/flutter_hooks.dart' show HookWidget, useState;
+
+import '../../flutter_utils.dart';
+
+// @freezed
+// class SelectOption{
+//   factory SelectOption.enums()
+// }
+
+class ButtonSelect<T> extends HookWidget {
+  const ButtonSelect({
+    Key? key,
+    required this.options,
+    required this.selected,
+    required this.onChange,
+    this.asString,
+    this.alwaysButtons = false,
+    this.wrapHorizontal = false,
+  }) : super(key: key);
+
+  final Iterable<T> options;
+  final T? selected;
+  final String Function(T)? asString;
+  final void Function(T) onChange;
+  final bool alwaysButtons;
+  final bool wrapHorizontal;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final isDropdown = useState(false);
+    final checkedShouldBeDropdown = useState(false);
+    double? buttonTop;
+
+    String _asString(T e) =>
+        asString == null ? e.toString() : asString!.call(e);
+
+    if (isDropdown.value && !alwaysButtons) {
+      return Align(
+        child: CustomDropdownField(
+          options: options,
+          selected: selected,
+          asString: _asString,
+          onChange: onChange,
+        ),
+      );
+    }
+
+    Widget _buildButtons(List<Widget> children) {
+      if (wrapHorizontal) {
+        return Wrap(
+          alignment: WrapAlignment.center,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          runAlignment: WrapAlignment.center,
+          children: children,
+        );
+      } else {
+        return ButtonBar(
+          alignment: MainAxisAlignment.center,
+          layoutBehavior: ButtonBarLayoutBehavior.constrained,
+          buttonPadding: EdgeInsets.zero,
+          children: children,
+        );
+      }
+    }
+
+    return Visibility(
+      // TODO: can be calculate when we need a dropdown?
+      visible: options.length <= 3 || checkedShouldBeDropdown.value,
+      maintainState: true,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: _buildButtons(
+          options.map((e) {
+            final s = _asString(e);
+
+            return TextButton(
+              key: Key(s),
+              onPressed: () => onChange(e),
+              style: TextButton.styleFrom(
+                  backgroundColor:
+                      e == selected ? theme.colorScheme.primary : null,
+                  primary: e == selected ? theme.colorScheme.onPrimary : null),
+              child: Builder(builder: (context) {
+                SchedulerBinding.instance!.addPostFrameCallback((timeStamp) {
+                  // print("Text ${ctx.size} ${ctx.globalPaintBounds}");
+                  if (buttonTop == null) {
+                    buttonTop = globalPaintBounds(context)!.top;
+                    return;
+                  }
+                  if (!checkedShouldBeDropdown.value) {
+                    if (buttonTop != globalPaintBounds(context)!.top) {
+                      isDropdown.value = options.length > 3;
+                    }
+                    checkedShouldBeDropdown.value = true;
+                  }
+                });
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                  child: Text(s),
+                );
+              }),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomDropdownField<T> extends HookWidget {
+  final Iterable<T> options;
+  final T? selected;
+  final String Function(T) asString;
+  final void Function(T) onChange;
+  final EdgeInsetsGeometry? padding;
+
+  const CustomDropdownField({
+    Key? key,
+    required this.selected,
+    required this.asString,
+    required this.onChange,
+    required this.options,
+    this.padding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isHovering = useState(false);
+
+    final _baseColor = Theme.of(context).colorScheme.onSurface;
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onEnter: (_) => isHovering.value = true,
+      onExit: (_) => isHovering.value = false,
+      child: Container(
+        decoration: BoxDecoration(
+          color: isHovering.value
+              ? _baseColor.withOpacity(0.08)
+              : _baseColor.withOpacity(0.05),
+          borderRadius: BorderRadius.circular(3),
+        ),
+        padding: padding ??
+            const EdgeInsets.only(
+              left: 8,
+              right: 8,
+              bottom: 8,
+              top: 7,
+            ),
+        child: DropdownButton<T>(
+          value: selected,
+          isExpanded: true,
+          isDense: true,
+          items: options.map((e) {
+            final s = asString(e);
+            return DropdownMenuItem<T>(
+              value: e,
+              child: Center(
+                child: Text(
+                  s,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            );
+          }).toList(),
+          onChanged: (value) {
+            if (value != null) onChange(value);
+          },
+        ),
+      ),
+    );
+  }
+}
